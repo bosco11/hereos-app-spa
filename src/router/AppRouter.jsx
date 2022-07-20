67e497ede2150ddcd813a6a75699a2213736d8ff
bosco11 <bosco11jico100@gmail.com>
2022-07-20 11:36:24 -0600

    cambios

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,14 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import { LoginPage } from "../auth/pages/LoginPage";
-import { DcPage } from "../heroes/pages/DcPage";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
+import { Route, Routes } from "react-router-dom";
+import { LoginPage } from "../auth";
+import { HeroesRoutes } from "../heroes";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="marvel" element={<MarvelPage />} />
-        <Route path="dc" element={<DcPage />} />
         <Route path="login" element={<LoginPage />} />
-
-        <Route path="/" element={<Navigate to="/marvel" />} />
+        <Route path="/*" element={<HeroesRoutes />} />
       </Routes>
     </>
   );
