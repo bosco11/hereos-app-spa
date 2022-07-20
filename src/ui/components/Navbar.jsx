3fb46d6c8aed1cd333b1eb99a4f68c374b31782c
bosco11 <bosco11jico100@gmail.com>
2022-07-20 11:49:23 -0600

    navigate

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,12 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    navigate('/login', { replace: true });
+  };
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
@@ -22,7 +28,7 @@ export const Navbar = () => {
           >
             DC
           </NavLink>
-          <NavLink
+          {/*<NavLink
             className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
             to="/hero"
           >
@@ -33,7 +39,7 @@ export const Navbar = () => {
             to="/search"
           >
             Search
-          </NavLink>
+          </NavLink>*/}
         </div>
       </div>
 
@@ -41,7 +47,9 @@ export const Navbar = () => {
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-primary">bosco</span>
 
-          <button className="nav-item nav-link btn">Logout</button>
+          <button className="nav-item nav-link btn" onClick={onLogout}>
+            Logout
+          </button>
         </ul>
       </div>
     </nav>
